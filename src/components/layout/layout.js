8f1cb878df97112d7d2c73e8f09cf09c95955fe6
Marcus <Malven@users.noreply.github.com>
2019-10-21 21:28:50 +0200

    Layout (#25)
    * Cleaned up layout structure
    
    * Changed the layout some

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,83 +1,39 @@
 import React from 'react';
 import { Navbar } from '../nav/navbar';
-import BandcampIcon from '../../icons/bandcamp';
-import SpotifyIcon from '../../icons/spotify';
-import YouTubeIcon from '../../icons/youtube';
-import FacebookIcon from '../../icons/facebook';
 import useMedia from 'use-media';
+import { Header } from '../header/header';
+import { Socials } from '../socials/socials';
+import { TourDates } from '../tour-dates/tourDates';
 
 export const Layout = ({ children }) => {
   const isWide = useMedia({ minWidth: 1450 });
 
   return (
-    <div className="container">
-      <div className="legbah-grid-container">
+    <div className="legbah-grid-container">
+      <div className={`legbah-header ${!isWide ? 'header-logo__center' : ''}`}>
+        <Header />
+      </div>
+      <div className="legbah-social">
+        <Socials />
+      </div>
+      <div className="legbah-main"> {children}</div>
+      <div className="legbah-navbar">
+        <Navbar />
+      </div>
+      <div className="legbah-tour-dates">
         <div
-          className={`legbah-header ${!isWide ? 'header-logo__center' : ''}`}
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            textAlign: 'center',
+            alignItems: 'center'
+          }}
         >
-          {isWide ? (
-            <img
-              style={{ height: '250px' }}
-              src="/static/sun.png"
-              alt="sun of Legbah"
-            />
-          ) : null}
-
-          <img
-            style={{
-              height: '250px',
-              width: '500px'
-            }}
-            src="/static/legbah-inverted.png"
-            alt="logotype of Legbah"
-          />
-
-          {isWide ? (
-            <img
-              style={{ height: '250px' }}
-              src="/static/sun.png"
-              alt="sun of Legbah"
-            />
-          ) : null}
-        </div>
-        <div className="legbah-navbar">
-          <Navbar />
-        </div>
-        <div className="legbah-main">{children}</div>
-        <div className="legbah-social">
-          <div className="socials">
-            <a
-              href="https://legbahofficial.bandcamp.com/"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              <BandcampIcon size={48} className="social-icon" />
-            </a>
-            <a
-              href="https://open.spotify.com/artist/2FFMu56Q0lpQceBNMtlpRk"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              <SpotifyIcon size={48} className="social-icon" />
-            </a>
-            <a
-              href="https://www.youtube.com/channel/UCtWBq19ICCRieDbF2yyiPXw"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              <YouTubeIcon size={48} className="social-icon" />
-            </a>
-            <a
-              href="https://www.facebook.com/Legbah/"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              <FacebookIcon size={48} className="social-icon" />
-            </a>
-          </div>
-        </div>
-        <div className="legbah-tour-dates">
-          <div style={{ height: '400px' }}></div>
+          <h2 style={{ width: '65%' }}>
+            Tour Header
+            <hr />
+          </h2>
+          <TourDates />
         </div>
       </div>
     </div>
---
+++ src/components/tour-dates/tourDates.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const TourDates = () => (
+  <div style={{ height: '400px', width: '65%' }}>Tour dates here</div>
+);
