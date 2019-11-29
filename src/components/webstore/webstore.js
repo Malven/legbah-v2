58b078b15cc987cb59e9763ad90d807a6748d580
Malven <marcusalven@gmail.com>
2019-11-29 21:49:24 +0100

    closes #18 - added dummy webstore for now

---
+++ src/components/webstore/webstore.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const WebStore = () => {
+  return (
+    <div className="flex flex-col p-5">
+      <h1 className="self-center font-display">Store</h1>
+      <a href="https://www.google.com">Go to webstore</a>
+    </div>
+  );
+};
---
+++ src/pages/store.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { WebStore } from '../components/webstore/webstore';
+
+const Store = () => {
+  return <WebStore />;
+};
+
+export default Store;
