a06c6dfba197a8e7627b879524f68b0a508c8633
Malven <marcusalven@gmail.com>
2020-06-03 21:01:23 +0200

    Centered tours

--- src/components/tour-dates/tourDates.js
+++ src/components/tour-dates/tourDates.js
@@ -5,7 +5,7 @@ import { useAppState } from '../contexts/app/useAppState';
 export const TourDates = () => {
   const { getTours } = useAppDispatch();
   const {
-    data: { tours }
+    data: { tours },
   } = useAppState();
 
   useEffect(() => {
@@ -20,12 +20,12 @@ export const TourDates = () => {
 
   return (
     <div className="flex flex-col pt-5">
-      <h1 className="self-center font-display text-4xl">Tour dates</h1>
+      <h1 className="self-center text-4xl font-display">Tour dates</h1>
       <hr className="mb-1" />
-      <ul className="list-inside mx-1">
+      <ul className="mx-1 list-inside">
         {tours &&
           tours.map(tour => (
-            <li key={tour.tourId} className="flex">
+            <li key={tour.tourId} className="flex justify-center">
               <span className="flex-shrink-0 text-legbah-gray">{`${tour.liveDate}:`}</span>
               <span className="ml-1">{tour.location}</span>
               <span className="ml-1">{tour.city}</span>
