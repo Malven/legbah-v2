bc30b1ccaded555fbbdb0a9bafe024569b25b164
Malven <marcusalven@gmail.com>
2020-06-04 13:24:50 +0200

    Removed unused packages

--- src/components/socials/socials.js
+++ src/components/socials/socials.js
@@ -11,33 +11,33 @@ export const Socials = () => {
         href="https://www.facebook.com/Legbah/"
         target="_blank"
         rel="noreferrer noopener"
+        aria-label="Facebook"
       >
         <FacebookIcon size={48} className="social-icon " />
-        {/* <FacebookIcon size={24} className="hidden ml-1 social-icon lg:block" /> */}
       </a>
       <a
         href="https://legbahofficial.bandcamp.com/"
         target="_blank"
         rel="noreferrer noopener"
+        aria-label="Bandcamp"
       >
         <BandcampIcon size={48} className="social-icon " />
-        {/* <BandcampIcon size={24} className="hidden ml-1 social-icon lg:block" /> */}
       </a>
       <a
         href="https://open.spotify.com/artist/2FFMu56Q0lpQceBNMtlpRk"
         target="_blank"
         rel="noreferrer noopener"
+        aria-label="Spotify"
       >
         <SpotifyIcon size={48} className="social-icon " />
-        {/* <SpotifyIcon size={24} className="hidden ml-1 social-icon lg:block" /> */}
       </a>
       <a
         href="https://www.youtube.com/channel/UCtWBq19ICCRieDbF2yyiPXw"
         target="_blank"
         rel="noreferrer noopener"
+        aria-label="Youtube"
       >
         <YouTubeIcon size={48} className="social-icon " />
-        {/* <YouTubeIcon size={24} className="hidden ml-1 social-icon lg:block" /> */}
       </a>
     </div>
   );
